20187aa03fd876e437f4110a4e1f99c3d7d7c24a
Ayush Pal <ayushpal15326@gmail.com>
2024-12-28 17:41:49 +0530

    React Routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,33 @@ import { Home } from "./page/Home";
 import { About } from "./page/About";
 import { Movie } from "./page/Movie";
 import { Contact } from "./page/Contact";
+import AppLayout from "./components/layout/AppLayout";
 const App = () => {
 
   const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home/>,
+    path:"/",
+    element:<AppLayout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+      {
+        path:"/movie",
+        element:<Movie/>
+      },
+      {
+        path:"/contact",
+        element:<Contact/>
+      }
+    ]
   },
-  {
-    path:"/about",
-    element:<About/>
-  },
-  {
-    path:"/movie",
-    element:<Movie/>
-  },
-  {
-    path:"/contact",
-    element:<Contact/>
-  }
+ 
   ])
   return <RouterProvider router={router} />
 }
